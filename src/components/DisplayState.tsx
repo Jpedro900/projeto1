aa02b45bebe22ebfde3989d366a7f3e31a62ee0c
Jpedro900 <jpedro.cs09@gmail.com>
2024-12-29 03:12:28 -0300

    Aula 19

--- src/components/DisplayState.tsx
+++ src/components/DisplayState.tsx
@@ -1,18 +1,26 @@
 interface DisplayStateProps {
   valor: number;
-  fadd: any;
-  fsub: any;
+  fvalor: any;
 }
 
 export default function DisplayState(props: DisplayStateProps) {
+
+  function operacao ( op : number ){
+    let c=props.valor
+    c+=op
+    props.fvalor(c)
+  }
+
   return (
     <div className="flex flex-col p-5 rounded-md bg-zinc-300 justify-center items-center w-[300px]">
-      <div className="flex bg-zinc-700 text-green-500 justify-center items-center w-[200px] mb-3">{props.valor}</div>
+      <div className="flex bg-zinc-700 text-green-500 justify-center items-center w-[200px] mb-3">
+        {props.valor}
+      </div>
       <div className=" flex gap-5">
-        <button className=" btnPadrao" onClick={props.fadd}>
+        <button className=" btnPadrao" onClick={()=>operacao(+1)}>
           somar
         </button>
-        <button className=" btnPadrao" onClick={props.fsub}>
+        <button className=" btnPadrao" onClick={()=>operacao(-1)}>
           subtrair
         </button>
       </div>
--- src/pages/useState/UseState.tsx
+++ src/pages/useState/UseState.tsx
@@ -4,21 +4,10 @@ import DisplayState from "@/components/DisplayState";
 export default function UseState() {
   let [cont, setCont] = useState<number>(0);
 
-  function add() {
-    let c = cont;
-    c++;
-    setCont(c);
-  }
-  function sub() {
-    let c = cont;
-    c--;
-    setCont(c);
-  }
-
   return (
     <div>
       UseState
-      <DisplayState valor={cont} fadd={add} fsub={sub}></DisplayState>
+      <DisplayState valor={cont} fvalor={setCont}></DisplayState>
     </div>
   );
 }
